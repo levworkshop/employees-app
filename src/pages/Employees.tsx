2c1d98264097eb5a86917dc86f8511f2e8be6ac7
levworkshop <noreply@githubusers.com>
2023-01-12 20:20:58 +0200

    display employees in table

--- src/pages/Employees.tsx
+++ src/pages/Employees.tsx
@@ -1,12 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { IEmployee } from "./types";
 
 function Employees() {
+    const [employees, setEmployees] = useState<Array<IEmployee>>([]);
+
     useEffect(() => {
         fetch('http://localhost/employees-app/api/get_employees.php')
             .then(res => res.json())
             .then(json => {
-                console.log(json);
+                setEmployees(json);
             })
     }, []);
 
@@ -24,7 +27,7 @@ function Employees() {
             <table className="table">
                 <thead>
                     <tr>
-                        <th>Employee Id</th>
+                        <th>Employee ID</th>
                         <th>First Name</th>
                         <th>Last Name</th>
                         <th>Address</th>
@@ -32,7 +35,24 @@ function Employees() {
                         <th></th>
                     </tr>
                 </thead>
-                <tbody></tbody>
+                <tbody>
+                    {employees.map(employee =>
+                        <tr key={employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.lastName}</td>
+                            <td>{employee.address}, {employee.cityId}</td>
+                            <td>{employee.active}</td>
+                            <td>
+                                <button
+                                    className="btn btn-default"
+                                >
+                                    <i className="bi-trash" />
+                                </button>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
             </table>
         </>
     );
